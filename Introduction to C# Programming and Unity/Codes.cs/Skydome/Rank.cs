f340d33938600f5211f110e381c9f56fc37ca415
Gustavo Borges <79589163+gBorges19@users.noreply.github.com>
2021-04-06 09:26:02 -0300

    Add files via upload

---
+++ Introduction to C# Programming and Unity/Codes.cs/Skydome/Rank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skydome
+{
+    /// <summary>
+    /// An enumeration for card ranks
+    /// </summary>
+    public enum Rank
+    {
+        Legendary,
+        SuperRare,
+        Rare,
+        Incomum,
+        Comum,
+
+    }
+}
---
+++ Introduction to C# Programming and Unity/Codes.cs/Skydome/Suit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skydome
+{
+    /// <summary>
+    /// An enumeration for card suits
+    /// </summary>
+    public enum Suit
+    {
+        Ellie,
+        Sula,
+        Egon,
+        Verna,
+        Hulgur,
+        Sagharo,
+        Groma,
+        Kreg,
+        Krub,
+        Adara,
+        Gardenias,
+    }
+}
