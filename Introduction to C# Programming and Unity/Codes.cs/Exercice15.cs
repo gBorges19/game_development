a1ffa78144011c563f028e43013b0b63c9ffa8a6
Gustavo Borges <79589163+gBorges19@users.noreply.github.com>
2021-04-07 16:39:05 -0300

    Add files via upload

---
+++ Introduction to C# Programming and Unity/Codes.cs/Exercice15.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Exercice15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("∗∗∗∗∗∗∗∗∗∗∗∗∗∗");
+            Console.WriteLine();
+            Console.WriteLine("Menu: ");
+            Console.WriteLine();
+            Console.WriteLine("1 - NewGame");
+            Console.WriteLine();
+            Console.WriteLine("2 - LoadGame");
+            Console.WriteLine();
+            Console.WriteLine("3 - Options");
+            Console.WriteLine();
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine();
+            Console.WriteLine("∗∗∗∗∗∗∗∗∗∗∗∗∗∗");
+            Console.WriteLine();
+            Console.WriteLine("Enter your choice (1, 2, 3 or 4): ");
+
+            int escolha = int.Parse(Console.ReadLine());
+            if (escolha == 1)
+            {
+                Console.WriteLine("Creating New Game . . .");
+            }
+            else if (escolha == 2)
+            {
+                Console.WriteLine("Loading Game . . .");
+            }
+            else if (escolha == 3)
+            {
+                Console.WriteLine("Setting Options . . .");
+            }
+            else if (escolha == 4)
+            {
+                Console.WriteLine("Leaving the Game . . .");
+
+            }
+            else
+            {
+                Console.WriteLine("DORK!!!");
+            }
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("Creating New Game . . .");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading Game . . .");
+                    break;
+                case 3:
+                    Console.WriteLine("Setting Options . . .");
+                    break;
+                case 4:
+                    Console.WriteLine("Leaving the Game . . .");
+                    break;
+                default:
+                    Console.WriteLine("DORK!!!");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
