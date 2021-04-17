2734fa5427fdeb93ff1d9565854100d41f3880a5
Gustavo Borges <79589163+gBorges19@users.noreply.github.com>
2021-04-17 01:17:58 -0300

    Add files via upload

---
+++ Introduction to C# Programming and Unity/Assets/E10-15/Assets/Scripts/Exercice11/PurpleTeddyBear.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleTeddyBear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  3x width  and  height
+        Vector3 newScale = transform.localScale;
+        newScale.x *= 3;
+        transform.localScale = newScale;
+    }
+
+}
